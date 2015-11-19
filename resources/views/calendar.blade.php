ce7f6b2ac5a81a69072d07a055705d46a00a1bbd
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2015-11-19 17:50:29 +0000

    Added some initial nav text

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -30,16 +30,43 @@
                 display: inline-block;
             }
 
+            .nav {
+                width: 50%;
+                display: inline-block;
+            }
+
+            .calendar {
+                width: 100%;
+            }
+
+            .left {
+                text-align: left;
+            }
+
+            .right {
+                text-align: right;
+            }
+
             .title {
                 font-size: 96px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Calendar Page</div>
-            </div>
+        <div class="nav left">
+            <span>[^]</span>
+            <span>day</span>
+            <span>week</span>
+            <span>month</span>
+            <span>toolbar</span>
+            <span>search</span>
+        </div>
+        <div class="nav right">
+            <span>user@email.com</span>
+            <span><a href="/">logout</a></span>
+        </div>
+        <div class="calendar">
+            <div class="title">Calendar Page</div>
         </div>
     </body>
 </html>
