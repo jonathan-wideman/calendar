149ffc5987b7a58b881206a9712d03be201f1be4
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2015-11-24 18:48:14 +0000

    Added create events table migration

---
+++ database/migrations/2015_11_24_182447_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->bigInteger('timeranges_id');
+            $table->bigInteger('calendars_id');
+            $table->bigInteger('colors_id');
+            $table->bigInteger('icons_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
