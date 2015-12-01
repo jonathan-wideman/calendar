e55b4d804cba75e8b2f5fbcad57e5681a0af37f7
Scott Henderson <hendscot@gmail.com>
2015-12-01 12:29:03 -0500

    Login functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,8 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $redirectPath = '/calendar';
+    protected $loginPath = '/';
     /**
      * Create a new authentication controller instance.
      *
