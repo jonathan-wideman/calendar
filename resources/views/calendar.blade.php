557554cf34957a6a2513c2cf902b1f17ab797e5f
Luke Behnke <Luke Behnke>
2015-12-14 11:42:37 -0500

    Changed font

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -23,7 +23,7 @@
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: "Lucida Console", Monospace;
+                font-family: Verdana, Geneva, sans-serif;
             }
 
             .container {
